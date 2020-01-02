ce7e6c9ae59e5aa54cdf05691f69a4ee4513f957
Anthony Eriksen <ajrayid@gmail.com>
2020-01-02 12:36:57 -0700

    Course outdated, cannot figure out how to add font awesome icons.

--- contactmanager/src/components/Contact.js
+++ contactmanager/src/components/Contact.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHome } from "@fortawesome/free-solid-svg-icons";
 
 class Contact extends Component {
   render() {
@@ -8,6 +10,7 @@ class Contact extends Component {
     return (
       <div className="card card-body mb-3">
         <h4>{name}</h4>
+        <FontAwesomeIcon icon={faHome} />
         <ul className="list-group">
           <li className="list-group-item">{email}</li>
           <li className="list-group-item">{phone}</li>
