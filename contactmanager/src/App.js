a95a434eb49073cf92b78fdf2fcc07fc25f951d4
Anthony Eriksen <ajrayid@gmail.com>
2020-01-02 11:10:51 -0700

    Adding State

--- contactmanager/src/App.js
+++ contactmanager/src/App.js
@@ -1,25 +1,21 @@
-import React from 'react';
-import Contact from './components/Contact';
+import React, { Component } from 'react';
+import Contacts from './components/Contacts';
 import Header from './components/Header';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
-function App() {
-	return (
-		<div className="App">
-			<Header branding="Contact Manager" />
-			<div className="container">
-				<Contact name="John Doe"
-					email="jdoe@gmail.com"
-					phone="555-555-5555" />
-
-				<Contact name="Anthony Eriksen"
-					email="aerik@gmail.com"
-					phone="777-777-7777" />
+class App extends Component {
+	render() {
+		return (
+			<div className="App">
+				<Header branding="Contact Manager" />
+				<div className="container">
+					<Contacts />
+				</div>
 			</div>
-		</div >
-	);
+		)
+	}
 }
 
 export default App;
